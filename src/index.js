9405622a18258f42ef67094415c1d8ca24c2ec81
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-07-11 13:40:56 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (_req,res) => {
-    res.send("<h1>BMG, Hello My Friend!!!</h1>")
+    res.send("<h1>BMG, Hello My Friend!</h1>")
 });
 
 app.listen(port, ()=>console.log(`running on port ${port}`));
