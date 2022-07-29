01052f066ce14c7a7b1ee45e7beb485758b38346
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-07-29 11:44:37 -0400

    updated

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -10,5 +10,50 @@ exports.getAllPosts = async (_req, res) => {
             },
         });
     }
-    catch (e) { console.log(e) };
+    catch (e) { res.status(400); console.log(e); };
+};
+exports.getOnePost = async (req, res) => {
+    try { 
+        const post = await Post.findById(req.params.id);
+        res.status(200).json({
+            data: {
+                post,
+            },
+        });
+    }
+    catch (e) { res.status(400); console.log(e); };
+};
+exports.createPost = async (req, res) => {
+    try { 
+        const post = await Post.create(req.body);
+        res.status(200).json({
+            data: {
+                post,
+            },
+        });
+    }
+    catch (e) { res.status(400); console.log(e); };
+};
+exports.updatePost = async (req, res) => {
+    try { 
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).json({
+            data: {
+                post,
+            },
+        });
+    }
+    catch (e) { res.status(400); console.log(e); };
+};
+exports.deletePost = async (req, res) => {
+    try { 
+        const post = await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            data: null,
+        });
+    }
+    catch (e) { res.status(400); console.log(e); };
 };
