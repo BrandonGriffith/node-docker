0733b656b47eae1e6a60b8b1a4bf9affdc3e4677
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-07-30 10:28:34 -0400

    updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,12 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
 
 exports.signUp = async (req, res) => {
     try {
+        if (req.body["password"]){
+            req.body["password"] = await bcrypt.hash(req.body["password"], 12);
+        }
         const newUser = await User.create(req.body);
         res.status(200).json({
             result: "success",
