ddb7fea77fc7180f50292db641ebba14915095d3
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-07-30 15:31:10 -0400

    updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,3 +17,56 @@ exports.signUp = async (req, res) => {
     }
     catch (e) { res.status(400).json({result:"fail", error:e}) };
 };
+exports.getAllUsers = async (_req, res) => {
+    try { 
+        const users = await User.find();
+        res.status(200).json({
+            result: "success",
+            results: users.length,
+            data: {
+                users,
+            },
+        });
+    }
+    catch (e) { res.status(400).json({result:"fail", error: e}); console.log(e); };
+};
+exports.getOneUser = async (req, res) => {
+    try { 
+        const user = await User.findById(req.params.id);
+        res.status(200).json({
+            result: "success",
+            data: {
+                user,
+            },
+        });
+    }
+    catch (e) { res.status(400).json({result:"fail", error: e}); console.log(e); };
+};
+exports.updateUser = async (req, res) => {
+    try {
+        if (req.body["password"]){
+            req.body["password"] = await bcrypt.hash(req.body["password"], 12);
+        };
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).json({
+            result: "success",
+            data: {
+                user,
+            },
+        });
+    }
+    catch (e) { res.status(400).json({result:"fail", error: e}); console.log(e); };
+};
+exports.deleteUser = async (req, res) => {
+    try { 
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            result: "success",
+            data: null,
+        });
+    }
+    catch (e) { res.status(400).json({result:"fail", error: e}); console.log(e); };
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-
+router.route("/").get(userController.getAllUsers);
 router.post("/signup", userController.signUp);
+router.route("/:id").get(userController.getOneUser).patch(userController.updateUser).delete(userController.deleteUser);
+
+
 module.exports = router;
