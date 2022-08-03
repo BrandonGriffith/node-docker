c365d0727b1ae13efbe50620681edeb548ccba18
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-08-03 15:05:31 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,59 +1,17 @@
-const express = require("express");
-const app = express();
-const port = process.env.EXPRESSPORT;
-app.use(express.json());
-app.enable("trust proxy");
-app.listen(port, ()=>console.log(`Express is listening on port ${port}`));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-// const {auth} = require("./middleware/authMiddleware");
-const postRouter = require("./routes/postRoutes");
-const userRouter = require("./routes/userRoutes");
-
-
-const cors = require("cors");
-const corsOptions = {
-    origin: process.env.CORS_URL,
-    credentials: true,
-};
-app.use(cors(corsOptions));
-
-
-const session = require("express-session");
-const RedisStore = require("connect-redis")(session);
-const { createClient } = require("redis");
-const redisClient = createClient({ legacyMode: true, url: process.env.REDIS_URL, });
-redisClient.connect().catch(console.error);
-redisClient.on('connect', (_err) => console.log('Redis connected'));
-const mySession = session({
-    key: "sessionCookie",
-    store: new RedisStore({ client: redisClient }),
-    secret: process.env.REDIS_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        httpOnly: false,
-        maxAge: 30000,
-        },
-    },
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-app.use(mySession);
 
-
-const mongoose = require("mongoose");
-const mongoUser = process.env.MONGO_INITDB_ROOT_USERNAME;
-const mongoPass = process.env.MONGO_INITDB_ROOT_PASSWORD;
-const connectToMongooseDB = () => {
-    mongoose.connect(`mongodb://${mongoUser}:${mongoPass}@mongo:27017/?authSource=admin`)
-    .then(()=>console.log("MongoDB connected"))
-    .catch((e)=>{console.log(e); setTimeout(connectToMongooseDB, 3000)});
-};
-connectToMongooseDB();
-
-
-// app.get("/", (_req,res) => res.send("<h1>BMG, Hello My Friend!</h1>"));
-app.route("/api/v1").get((_req,res) => res.send("<h1>BMG, Hello My Friend. This is a test.</h1>"));
-app.use("/api/v1/posts", postRouter);
-app.use("/api/v1/users", userRouter);
-module.exports = app;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const port = process.env.EXPRESSPORT;
+app.use(express.json());
+app.enable("trust proxy");
+app.listen(port, ()=>console.log(`Express is listening on port ${port}`));
+
+
+// const {auth} = require("./middleware/authMiddleware");
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
+
+const cors = require("cors");
+const corsOptions = {
+    origin: process.env.CORS_URL,
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
+const { createClient } = require("redis");
+const redisClient = createClient({ legacyMode: true, url: process.env.REDIS_URL, });
+redisClient.connect().catch(console.error);
+redisClient.on('connect', (_err) => console.log('Redis connected'));
+const mySession = session({
+    key: "sessionCookie",
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.REDIS_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        httpOnly: false,
+        maxAge: 30000,
+        },
+    },
+);
+app.use(mySession);
+
+
+const mongoose = require("mongoose");
+const mongoUser = process.env.MONGO_INITDB_ROOT_USERNAME;
+const mongoPass = process.env.MONGO_INITDB_ROOT_PASSWORD;
+const connectToMongooseDB = () => {
+    mongoose.connect(`mongodb://${mongoUser}:${mongoPass}@mongo:27017/?authSource=admin`)
+    .then(()=>console.log("MongoDB connected"))
+    .catch((e)=>{console.log(e); setTimeout(connectToMongooseDB, 3000)});
+};
+connectToMongooseDB();
+
+
+// app.get("/", (_req,res) => res.send("<h1>BMG, Hello My Friend!</h1>"));
+app.route("/api/v1").get((_req,res) => res.send("<h1>BMG, Hello My Friend. This is a test.</h1>"));
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
+module.exports = app;
